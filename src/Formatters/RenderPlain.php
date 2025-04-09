c9e7ad10133f0c3804024ce705ee2a97f2cf5c2f
aabelyaev <a.belyaev87@bk.ru>
2025-04-09 09:02:41 +0500

    change

--- src/Formatters/RenderPlain.php
+++ src/Formatters/RenderPlain.php
@@ -7,40 +7,35 @@ function renderPlain(array $tree)
     return buildPlain($tree);
 }
 
-function buildPlain(array $tree, string $parent = '')
+function buildPlain($tree, $parent = '')
 {
     $nodesForPlain = array_map(function ($node) use ($parent) {
         switch ($node['type']) {
             case 'nested':
                 return buildPlain($node['children'], "{$parent}{$node['key']}.");
             default:
-                return formatNode($node, $parent);
+                $str = "Property '%s'";
+                $action = '';
+                switch ($node['type']) {
+                    case 'changed':
+                        $action = " was changed. From '%s' to '%s'";
+                        break;
+                    case 'removed':
+                        $action = " was removed";
+                        break;
+                    case 'added':
+                        $str .= " was added with value: '%s'";
+                        break;
+                    default:
+                        throw new \Exception("Unknown node {$node}");
+                }
+                return sprintf($str . $action, "{$parent}{$node['key']}", stringify($node['dataBefore']), stringify($node['dataAfter']));
         }
     }, $tree);
     return implode(PHP_EOL, array_filter($nodesForPlain, fn($item) => !empty($item)));
 }
 
-function formatNode(array $node, string $parent): string
-{
-    $messageTemplate = "Property '%s' %s";
-    $action = '';
-    switch ($node['type']) {
-        case 'changed':
-            $action = sprintf("was changed. From '%s' to '%s'", stringify($node['dataBefore']), stringify($node['dataAfter']));
-            break;
-        case 'removed':
-            $action = "was removed";
-            break;
-        case 'added':
-            $action = sprintf("was added with value: '%s'", stringify($node['dataAfter']));
-            break;
-        default:
-            throw new \Exception("Unknown node type: {$node['type']}");
-    }
-    return sprintf($messageTemplate, "{$parent}{$node['key']}", $action);
-}
-
-function stringify($value): string
+function stringify($value)
 {
     if (is_bool($value)) {
         return $value ? 'true' : 'false';
@@ -50,5 +45,5 @@ function stringify($value): string
         return 'complex value';
     }
 
-    return (string)$value;
+    return $value;
 }
