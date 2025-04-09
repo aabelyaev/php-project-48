80e96089e6d2d5ad6962d863993dcf4655395760
aabelyaev <a.belyaev87@bk.ru>
2025-04-09 07:50:24 +0500

    change

--- src/Formatters/RenderPretty.php
+++ src/Formatters/RenderPretty.php
@@ -20,38 +20,33 @@ function buildPretty($tree, $level = 0)
                 return implode('', [INDENT, $node['key'], ': ', $newChildren]);
             case 'unchanged':
                 $valueStr = stringify($node['dataAfter']); 
-                return implode('', [$offset, INDENT, $node['key'], ': ', $valueStr]);
-                case 'changed':
-                    $beforeValueStr = stringify($node['dataBefore']);
-                    $afterValueStr = stringify($node['dataAfter']);
-                    return [
-                        sprintf('%s  + %s: %s', $offset, $node['key'], $afterValueStr),
-                        "\n",
-                        sprintf('%s  - %s: %s', $offset, $node['key'], $beforeValueStr),
-                    ];
-                
-                case 'removed':
-                    $valueStr = stringify($node['dataBefore']);
-                    return [
-                        sprintf('%s  - %s: %s', $offset, $node['key'], $valueStr),
-                    ];
-                
-                case 'added':
-                    $valueStr = stringify($node['dataAfter']);
-                    return [
-                        sprintf('%s  + %s: %s', $offset, $node['key'], $valueStr),
-                    ];
-                default:
-                    return "Unknown node type: {$node['type']}";
+               return implode('', [$offset, INDENT, $node['key'], ': ', $valueStr]);
+            case 'changed':
+                $beforeValueStr = stringify($node['dataBefore']);
+                $afterValueStr = stringify($node['dataAfter']);
+                return $offset
+                    . "  + {$node['key']}: {$afterValueStr}" . "\n"
+                    . $offset
+                    . "  - {$node['key']}: {$beforeValueStr}";
+            case 'removed':
+                $valueStr = stringify($node['dataBefore']);
+                return $offset
+                    . "  - {$node['key']}: {$valueStr}";
+            case 'added':
+                $valueStr = stringify($node['dataAfter']);
+                return $offset
+                    . "  + {$node['key']}: {$valueStr}";
+            default:
+                return "Unknown node type: {$node['type']}";
         }
     }, $tree);
 
     $result = implode("\n", array_filter($nodesForPretty));
     
     if ($level == 0) {
-        return sprintf('{%s%s%s}', "\n", $result, "\n");
+        return "{" . "\n" . $result . "\n" . "}";
     } else {
-        return sprintf('%s{%s%s%s}', $offset, "\n", $result, "\n");
+        return $offset . "{\n" . $result . "\n}";
     }
 }
 
@@ -69,16 +64,16 @@ function stringify($value, $parentOffset = '', $level = 0)
     
     $keys = array_keys($value);
 
-    $nestedItems = array_map(function ($key) use ($parentOffset, $offset, $value) {
+    $nestedItem = array_map(function ($key) use ($parentOffset, $offset, $value) {
         $keyStr = $parentOffset ? $parentOffset . "  " : '';
         return "$keyStr$offset{$key}: {$value[$key]}";
     }, $keys);
 
-    $result = implode("\n", $nestedItems);
+    $result = implode("\n", $nestedItem);
     
     if ($level == 0 && !empty($parentOffset)) {
-        return sprintf('{%s%s%s}', "\n", $result, "\n" . $parentOffset);
+        return "{" . "\n" . $result . "\n" . $parentOffset . "}";
     } else {
-        return sprintf('{%s%s}', "\n", $offset . "{" . $result . "\n");
+        return "{" . "\n" . $result . "\n" . $offset;
     }
 }
