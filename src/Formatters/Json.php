fd8822823c844468636a976a61623d91bbe84421
aabelyaev <a.belyaev87@bk.ru>
2025-04-10 14:47:04 +0500

    change

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -4,12 +4,5 @@ namespace Differ\Formatters\Json;
 
 function perform(array $diff): string
 {
-    $json = json_encode($diff, JSON_PRETTY_PRINT);
-
-    if ($json === false) {
-        throw new \Exception("Failed to encode data as JSON");
-    }
-
-    return $json;
+    return json_encode($diff, JSON_PRETTY_PRINT);
 }
-
