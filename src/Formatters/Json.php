a3d70534ccfde97e591f3bbd1bf2e3503ce173ee
aabelyaev <a.belyaev87@bk.ru>
2025-04-11 08:18:15 +0500

    change

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -4,9 +4,9 @@ namespace Differ\Formatters\Json;
 
 function perform(array $diff): string
 {
-    $json = @json_encode($diff, JSON_THROW_ON_ERROR);
-    if ($json === false) {
-        throw new \Exception("Failed to encode data as JSON");
+    try {
+        return json_encode($diff, JSON_THROW_ON_ERROR);
+    } catch (\JsonException $e) {
+        throw new \Exception("Failed to encode data as JSON: " . $e->getMessage());
     }
-    return $json;
 }
